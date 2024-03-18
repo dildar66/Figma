f01e9ea7f9e8ef37edd67fab8e92c8f24dab0f49
dildar66 <dildaralidahri786@gmail.com>
2024-03-18 06:19:19 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+function toggle() {
+    let linkbox = document.getElementById('linkbox')
+    let iconbar = document .getElementById('iconbar');
+    if (linkbox.style.display != 'block') {
+        linkbox.style.display = 'block';
+    } else {
+        linkbox.style.display = 'none';
+    }
+}
